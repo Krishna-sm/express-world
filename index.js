4ea9d22a5d695e2db8e763ef0577fc950795a20e
Krishna Bansal <kana.sonkh@gmail.com>
2022-12-06 07:05:34 +0530

    ititalcommit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const app =express();
 const port = process.env.PORT || 3000;
+app.use(express.json());
 app.get('/',(req,res)=>{
     res.send("<h1>Hello world</h1>");
+})
+app.get('/about',(req,res)=>{
+    res.send(`<h1>Hello : ${req.body.name}</h1>`);
 })
 app.listen(port,()=>{
     console.log(`the app listern on ${port}`)
